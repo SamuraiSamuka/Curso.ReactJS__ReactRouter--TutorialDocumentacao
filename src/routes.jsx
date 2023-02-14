f3959fac3229da3699a657fd548942dbfeaeac26
Samuel Silva de Carvalho <samuel.carvalho.dev@gmail.com>
2023-02-14 14:03:58 -0300

    Client side routing habilitado e conexão ao DB em andamento

---
+++ src/routes.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./error-page";
+import './index.css'
+import Contact from "./routes/contact";
+import Root, {loader as rootLoader} from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contachtId",
+        element: <Contact />,
+      },
+    ]
+  },
+])
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
